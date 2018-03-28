b10c6a80f77396c820941f9fa438598ab1af21d4
Aisik <aisiknp@gmail.com>
2018-03-28 13:50:51 -0400

    Update U2A6_FunWithLoopsFrame.java

--- src/U2A6_FunWithLoops/U2A6_FunWithLoopsFrame.java
+++ src/U2A6_FunWithLoops/U2A6_FunWithLoopsFrame.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 28/03/2018
+ * Aisik Pilote
+ * FunWithLoops
+ * displays all integers between two integers determined by the user
  */
 
 package U2A6_FunWithLoops;
